a3c2a6b3ae11b12c6cb913e1f7fcd8ca3cbb33c6
nancy <smithnancy@outlook.com>
2023-06-15 12:02:15 +1000

    stying the register view,component link applied

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" novalidate>
         @csrf
 
         <!-- Name -->
@@ -39,14 +39,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="flex justify-between my-2">
+            <x-link :href="route('login')">
+                Log in
+            </x-link>
+            <x-link :href="route('register')">
+                Already registered?
+            </x-link>
         </div>
+        <x-primary-button class="w-full justify-center">
+            {{ __('Register') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
