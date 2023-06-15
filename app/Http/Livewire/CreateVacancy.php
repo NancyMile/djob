359e25b10c640b878fa09d4bd4377ac75157ef28
nancy <smithnancy@outlook.com>
2023-06-15 20:46:54 +1000

    livewire create vacancy adding the patrameter to livewire model for  validating, nme on view has been replaced  by wire:model

--- app/Http/Livewire/CreateVacancy.php
+++ app/Http/Livewire/CreateVacancy.php
@@ -4,10 +4,30 @@ namespace App\Http\Livewire;
 
 use App\Models\Category;
 use App\Models\Salary;
+use Doctrine\Inflector\Rules\English\Rules;
 use Livewire\Component;
 
 class CreateVacancy extends Component
 {
+    public $title;
+    public $salary;
+    public $category;
+    Public $company;
+    public $last_day;
+    public $description;
+    public $image;
+
+    protected $rules = [
+        'titulo' => 'required|string',
+        'salary' => 'required',
+        'category' => 'required',
+        'company' => 'required',
+        'last_day' => 'required',
+        'description' => 'required',
+        'image' => 'required',
+    ];
+
+
     public function render()
     {
         //query the db
--- resources/views/livewire/create-vacancy.blade.php
+++ resources/views/livewire/create-vacancy.blade.php
@@ -2,12 +2,12 @@
     <!-- Email Address -->
     <div>
         <x-input-label for="title" :value="__('Vacancy Title')" />
-        <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" placeholder="vacancy title" :value="old('title')" />
+        <x-text-input id="title" class="block mt-1 w-full" type="text" wire:model="title" placeholder="vacancy title" :value="old('title')" />
         <x-input-error :messages="$errors->get('title')" class="mt-2" />
     </div>
     <div>
         <x-input-label for="salary" :value="__('Salary')" />
-        <select name="role" id="salary" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300
+        <select wire:model="role" id="salary" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300
         focus:border-indigo-500 dark:focus:border-indigo-600
         focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">
             <option value="">-- Select--</option>
@@ -19,7 +19,7 @@
     </div>
     <div>
         <x-input-label for="category" :value="__('Category')" />
-        <select name="category" id="category" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300
+        <select wire:model="category" id="category" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300
         focus:border-indigo-500 dark:focus:border-indigo-600
         focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">
             <option value="">-- Select --</option>
@@ -32,25 +32,25 @@
     </div>
     <div>
         <x-input-label for="company" :value="__('Company')" />
-        <x-text-input id="company" class="block mt-1 w-full" type="text" name="company" placeholder="company" :value="old('company')" />
+        <x-text-input id="company" class="block mt-1 w-full" type="text" wire:model="company" placeholder="company" :value="old('company')" />
         <x-input-error :messages="$errors->get('company')" class="mt-2" />
     </div>
     <div>
         <x-input-label for="last_day" :value="__('Open until ')" />
-        <x-text-input id="last_day" class="block mt-1 w-full" type="date" name="last_day" :value="old('last_day')" />
+        <x-text-input id="last_day" class="block mt-1 w-full" type="date" wire:model="last_day" :value="old('last_day')" />
         <x-input-error :messages="$errors->get('last_day')" class="mt-2" />
     </div>
     <div>
         <x-input-label for="description" :value="__('description')" />
-        <textarea id="description" name="description" placeholder="description" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300
+        <textarea id="description" wire:model="description" placeholder="description" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300
         focus:border-indigo-500 dark:focus:border-indigo-600
         focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full h-72">{{old('description')}}</textarea>
         <x-input-error :messages="$errors->get('description')" class="mt-2" />
     </div>
     <div>
-        <x-input-label for="imagen" :value="__('imagen')" />
-        <x-text-input id="imagen" class="block mt-1 w-full" type="file" name="title" />
-        <x-input-error :messages="$errors->get('imagen')" class="mt-2" />
+        <x-input-label for="image" :value="__('image')" />
+        <x-text-input id="image" class="block mt-1 w-full" type="file" wire:model="image" />
+        <x-input-error :messages="$errors->get('image')" class="mt-2" />
     </div>
     <x-primary-button>
         {{ __('Create Vacancy') }}
