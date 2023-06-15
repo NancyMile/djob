4e341ce0113bb044428f7e95c2528f28ab757305
nancy <smithnancy@outlook.com>
2023-06-15 10:48:10 +1000

    input  error  messaje style  modified

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,7 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'text-sm text-red-500 round font-bold space-y-1 bg-red-100 border-l-4 p-2 border-red-600' ]) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
         @endforeach
