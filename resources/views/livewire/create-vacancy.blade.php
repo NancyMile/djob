be4c49c3be19b67629b4432eacb2c4ebc4b540d0
nancy <smithnancy@outlook.com>
2023-06-15 21:32:12 +1000

    livewire crear vacancy wire:model value fixed for salary

--- resources/views/livewire/create-vacancy.blade.php
+++ resources/views/livewire/create-vacancy.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div>
         <x-input-label for="salary" :value="__('Salary')" />
-        <select wire:model="role" id="salary" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300
+        <select wire:model="salary" id="salary" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300
         focus:border-indigo-500 dark:focus:border-indigo-600
         focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">
             <option value="">-- Select--</option>
