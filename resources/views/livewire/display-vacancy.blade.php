0fbb42ea410e6af05825b1103aa6c456584c6633
nancy <smithnancy@outlook.com>
2023-06-16 22:58:31 +1000

    vacancy model relations with salary and category created, now the show vire display saly and category properly

--- resources/views/livewire/display-vacancy.blade.php
+++ resources/views/livewire/display-vacancy.blade.php
@@ -9,10 +9,10 @@
                 Last Day: <span class="normal-case font-normal">{{ $vacancy->last_date->toFormattedDateString() }}</span>
             </p>
             <p class=" font-bold text-sm text-gray-800 my-3 uppercase">
-                Category: <span class="normal-case font-normal">{{ $vacancy->category_id }}</span>
+                Category: <span class="normal-case font-normal">{{ $vacancy->category->category }}</span>
             </p>
             <p class=" font-bold text-sm text-gray-800 my-3 uppercase">
-                Salary: <span class="normal-case font-normal">{{ $vacancy->salary_id }}</span>
+                Salary: <span class="normal-case font-normal">{{ $vacancy->salary->salary }}</span>
             </p>
             <p class=" font-bold text-sm text-gray-800 my-3 uppercase">
                 Company: <span class="normal-case font-normal">{{ $vacancy->company }}</span>
