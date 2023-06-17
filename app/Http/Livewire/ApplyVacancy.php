f284eefc4bbadb7bc9e274fa6de19916344f3e37
nancy <smithnancy@outlook.com>
2023-06-17 17:55:07 +1000

    apply vacancy form validation added

--- app/Http/Livewire/ApplyVacancy.php
+++ app/Http/Livewire/ApplyVacancy.php
@@ -6,6 +6,23 @@ use Livewire\Component;
 
 class ApplyVacancy extends Component
 {
+    public $resume;
+
+    protected $rules = [
+        'resume' => 'required|mines:pdf'
+    ];
+
+    public function apply()
+    {
+        $data = $this->validate();
+
+        //save resume on disk
+
+        //create notification and send email
+
+        //display a success message
+    }
+
     public function render()
     {
         return view('livewire.apply-vacancy');
--- resources/views/livewire/apply-vacancy.blade.php
+++ resources/views/livewire/apply-vacancy.blade.php
@@ -1,10 +1,13 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
     <h3 class="text-center text-2xl font-bold my-4">Apply to this vacancy</h3>
-    <form class="w-96 mt-5">
+    <form wire:submit.prevent='apply' class="w-96 mt-5">
         <div class="mb-4">
             <x-input-label for="resume" :value="__('resume (PDF)')" />
-            <input type="file" name="resume" id="resume" accept=".pdf" class="block mt-1 w-full"/>
+            <input type="file" wire:model="resume" id="resume" accept=".pdf" class="block mt-1 w-full"/>
         </div>
+        @error('resume')
+            <livewire:display-alert :message="$message"/>
+        @enderror
         <x-primary-button>
             {{ __('Send') }}
         </x-primary-button>
