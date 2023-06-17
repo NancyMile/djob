38c9108370a4d235dcc78c1437c4007e240bda48
nancy <smithnancy@outlook.com>
2023-06-17 17:42:24 +1000

    apply form  submit button added

--- resources/views/livewire/apply-vacancy.blade.php
+++ resources/views/livewire/apply-vacancy.blade.php
@@ -1,7 +1,12 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
     <h3 class="text-center text-2xl font-bold my-4">Apply to this vacancy</h3>
     <form class="w-96 mt-5">
-        <x-input-label for="resume" :value="__('resume (PDF)')" />
-        <input type="file" name="resume" id="resume" accept=".pdf" class="block mt-1 w-full">
+        <div class="mb-4">
+            <x-input-label for="resume" :value="__('resume (PDF)')" />
+            <input type="file" name="resume" id="resume" accept=".pdf" class="block mt-1 w-full"/>
+        </div>
+        <x-primary-button>
+            {{ __('Send') }}
+        </x-primary-button>
     </form>
 </div>
