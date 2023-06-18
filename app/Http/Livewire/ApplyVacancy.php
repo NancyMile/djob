dfa21331808eec9943fde9aec5dd3608b4a3f8a4
nancy <smithnancy@outlook.com>
2023-06-18 12:51:28 +1000

    applyVacancy resume stored on disk

--- app/Http/Livewire/ApplyVacancy.php
+++ app/Http/Livewire/ApplyVacancy.php
@@ -3,20 +3,27 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ApplyVacancy extends Component
 {
     public $resume;
 
     protected $rules = [
-        'resume' => 'required|mines:pdf'
+        'resume' => 'required|mimes:pdf'
     ];
+
+    use WithFileUploads;
 
     public function apply()
     {
         $data = $this->validate();
 
         //save resume on disk
+        $resume = $this->resume->store('public/resumes');
+        //dd($resume);//"public/resumes/resume.pdf"
+        $data['resume'] = str_replace('public/resume/','',$resume);
+
 
         //create notification and send email
 
