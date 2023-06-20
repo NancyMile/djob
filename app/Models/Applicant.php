873e0dedb0cba2455029378131d4182361b9e937
nancy <smithnancy@outlook.com>
2023-06-20 12:28:44 +1000

    Applicant model relationship with user added

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -13,4 +13,9 @@ class Applicant extends Model
         'vacancy_id',
         'resume'
     ];
+
+    // on table Applicants we have user_id,this relationship brings the info of the user
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
